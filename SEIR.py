a98b216df52ca3743867df659e34fd97b58fb63d
Travis Czechorski <tjczec01@gmail.com>
2020-03-27 17:05:31 -0400

    Update SEIR.py
    Fixed Vertical Peak Line

--- SEIR.py
+++ SEIR.py
@@ -7,7 +7,6 @@ Created on Thu Mar 26 22:55:37 2020
 
 import matplotlib.pyplot as plt 
 from scipy.integrate import solve_ivp
-# from tkinter import Tk, StringVar, ttk, N, W, E, S
 import os
 
 clear = lambda: os.system('cls')
@@ -54,17 +53,8 @@ Time = [i for i in range(0, int(Daysnn + 1), 1)]
 tt = list(range(0,its,1))
 Time_f = [i*itern for i in tt]
 Y0 = [NP, Infi, 0.0, 0.0]
-# RB = rb*Daysnn
-# print(R0(α, β, μ, λ))
-# print('Λ') 
-# print('μ') 
-# print(α)
-# print(β)
-# print(RB)
-# print(λ)
-# print("R0 = {}".format(β/λ))
 
-answer = solve_ivp(SEIR, Days, Y0, t_eval=Time_f, method = 'Radau', args=(α, β, μ, λ, Λ, NP))
+answer = solve_ivp(SEIR, Days, Y0, t_eval=Time_f, method = 'RK45', args=(α, β, μ, λ, Λ, NP))
 
 Sn = answer.y[0]
 En = answer.y[1]
@@ -81,7 +71,7 @@ Ip = [(i/NP)*100.0 for i in answer.y[2]]
 Rp = [(i/NP)*100.0 for i in answer.y[3]]
 
 m = max(In)
-mi = (In.tolist()).index(max(In.tolist()))
+mi = (In.tolist()).index(max(In))
 mip = mi/its
 peakn = round(Daysnn*mip)
 my = max(Ip)
@@ -94,7 +84,7 @@ plt.plot(Time_f, Sn, 'b-', label=r'$\it{Susceptible}$')
 plt.plot(Time_f, En, 'c-', label=r'$\it{Exposed}$')
 plt.plot(Time_f, In, 'r-', label=r'$\it{Infected}$')
 plt.plot(Time_f, Rn, 'g-', label=r'$\it{Recovered}$')
-plt.axvline(x=int(mi), color='k', linestyle='--')
+plt.axvline(x=int(Time_f[mi]), color='k', linestyle='--')
 plt.legend([r'$\it{Susceptible}$', r'$\it{Exposed}$', r'$\it{Infected}$', r'$\it{Recovered}$', r'$\it{}$'.format("{}{}{}".format('{',"Peak \ = {}{}{} \ Days".format('{',r'\ {}'.format(peakn),'}'),'}'))],loc="best", fontsize=15)
 plt.xlim((0,Daysnn))
 plt.ylim((0,NP))
@@ -112,7 +102,7 @@ plt.plot(Time_f, Sp, 'b-', label=r'$\it{Susceptible}$')
 plt.plot(Time_f, Ep, 'c-', label=r'$\it{Exposed}$')
 plt.plot(Time_f, Ip, 'r-', label=r'$\it{Infected}$')
 plt.plot(Time_f, Rp, 'g-', label=r'$\it{Recovered}$')
-plt.axvline(x=int(mi), color='k', linestyle='--')
+plt.axvline(x=int(Time_f[myi]), color='k', linestyle='--')
 plt.legend([r'$\it{Susceptible}$', r'$\it{Exposed}$', r'$\it{Infected}$', r'$\it{Recovered}$', r'$\it{}$'.format("{}{}{}".format('{',"Peak \ = {}{}{} \ Days".format('{',r'\ {}'.format(peakn),'}'),'}'))],loc="best", fontsize=15)
 plt.xlim((0,Daysnn))
 plt.ylim((0,100))
